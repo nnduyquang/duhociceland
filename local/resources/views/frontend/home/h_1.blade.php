3d2a00c9122e0f18004a5ce3a8e0ae1e600f0504
nnduyquang@gmail.com <bunvodich>
2019-03-12 15:50:33 +0700

    ok

--- local/resources/views/frontend/home/h_1.blade.php
+++ local/resources/views/frontend/home/h_1.blade.php
@@ -65,35 +65,43 @@
             </div>
             <div class="col-md-4  wow fadeInRight">
                 <div class="register">
-                    <h4>@lang('content.home_contact_title')</h4>
-                    <div class="ip-name input-group" style="display: block">
-                        <input name="name" type="text" placeholder="@lang('content.home_contact_name')" class="form-control input-text" style="display: inline-block;border-radius: unset">
-                        <div class="invalid-feedback" style="font-size: 15px;font-weight:  bold;">
-                            Please choose a username.
+                    <div class="bg-img" style="background-image:url({{URL::to('images/common/du-hoc-ireland-co-hoi-hoc-bong-hap-dan-danh-rieng-cho-sinh-vien-viet-nam.jpg')}});"></div>
+                    <div class="content">
+                        <h4>@lang('content.home_contact_title')</h4>
+                        <div class="ip-name input-group" style="display: block">
+                            <input name="name" type="text" placeholder="@lang('content.home_contact_name')"
+                                   class="form-control input-text" style="display: inline-block;border-radius: unset">
+                            <div class="invalid-feedback" style="font-size: 15px;font-weight:  bold;">
+                                Please choose a username.
+                            </div>
                         </div>
-                    </div>
-                    <div class="ip-phone input-group" style="display: block">
-                        <input name="phone" type="text" placeholder="@lang('content.home_contact_phone')" class="form-control input-text" style="display: inline-block;border-radius: unset">
-                        <div class="invalid-feedback" style="font-size: 15px;font-weight:  bold;">
-                            Please choose a username.
+                        <div class="ip-phone input-group" style="display: block">
+                            <input name="phone" type="text" placeholder="@lang('content.home_contact_phone')"
+                                   class="form-control input-text" style="display: inline-block;border-radius: unset">
+                            <div class="invalid-feedback" style="font-size: 15px;font-weight:  bold;">
+                                Please choose a username.
+                            </div>
                         </div>
-                    </div>
-                    <div class="ip-email input-group" style="display: block">
-                        <input name="email" type="text" placeholder="@lang('content.home_contact_email')" class="form-control input-text" style="display: inline-block;border-radius: unset">
-                        <div class="invalid-feedback" style="font-size: 15px;font-weight:  bold;">
-                            Please choose a username.
+                        <div class="ip-email input-group" style="display: block">
+                            <input name="email" type="text" placeholder="@lang('content.home_contact_email')"
+                                   class="form-control input-text" style="display: inline-block;border-radius: unset">
+                            <div class="invalid-feedback" style="font-size: 15px;font-weight:  bold;">
+                                Please choose a username.
+                            </div>
                         </div>
-                    </div>
-                    <textarea name="description" rows="4" cols="50" placeholder="@lang('content.home_contact_message')"></textarea>
-                    <div class="btn-submit">
-                        {{--<a href="#">đăng ký</a>--}}
-                        <div class="button-group">
-                            <button  type="button" class="btn btn-contact submit-re">@lang('content.home_contact_button')<i
-                                        class="fa fa-spinner fa-spin fa-3x fa-fw loadingSending" style="
+                        <textarea name="description" rows="4" cols="50"
+                                  placeholder="@lang('content.home_contact_message')"></textarea>
+                        <div class="btn-submit">
+                            {{--<a href="#">đăng ký</a>--}}
+                            <div class="button-group">
+                                <button type="button"
+                                        class="btn btn-contact submit-re">@lang('content.home_contact_button')<i
+                                            class="fa fa-spinner fa-spin fa-3x fa-fw loadingSending" style="
     font-size: 15px;display: none"></i><i
-                                        class="fa fa-check-circle successSending" aria-hidden="true"
-                                        style="display: none"></i></button>
-                            <span style="display: none;color:  green;font-weight:  bold;margin-top:  5px;">Chúng tôi đã nhận được mail và sẽ phản hồi quý khách trong 24h. Xin cảm ơn.</span>
+                                            class="fa fa-check-circle successSending" aria-hidden="true"
+                                            style="display: none"></i></button>
+                                <span style="display: none;color:  green;font-weight:  bold;margin-top:  5px;">Chúng tôi đã nhận được mail và sẽ phản hồi quý khách trong 24h. Xin cảm ơn.</span>
+                            </div>
                         </div>
                     </div>
                 </div>
--- local/resources/views/frontend/home/pop_up.blade.php
+++ local/resources/views/frontend/home/pop_up.blade.php
@@ -1,135 +1,61 @@
 <style>
-    #popup-hera {
-        width: 100%;
-        height: 100%;
-        background-color: rgba(00, 00, 00, 0.8);
-        position: fixed;
-        top: 0;
-        left: 0;
-        z-index: 100;
-        overflow: hidden;
-        text-align: center;
-    }
-
-    #popup-hera .popup-content {
-        width: 100%;
-        height: 100%;
-        /*background-color: #fff;*/
-        position: relative;
-    }
-
-    #popup-hera .popup-content .pop-center {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        width: 60%;
-        background-color: white;
-        padding: 25px;
-        background-repeat: no-repeat;
-        background-position: right;
-        background-attachment: scroll;
-        background-size: cover;
-        border-top-width: 1px;
-        border-right-width: 1px;
-        border-bottom-width: 1px;
-        border-left-width: 1px;
-
-    }
-    #popup-hera .popup-content .pop-center .one-item{
-        background-color: rgba(255,255,255,0.73);
-        padding: 25px;
-    }
-
-    #popup-hera .popup-content .pop-center .one-item h4 {
-        color: #004085;
-        font-weight: bold;
-        font-size: 25px;
-        text-align: left;
-    }
-
-    #popup-hera .popup-content .pop-center .one-item p {
-        padding: 20px 0;
-        font-size: 20px;
-        text-align: left;
-        color: rgb(102, 104, 120);
-    }
-
-    #popup-hera .popup-content .pop-center .one-item a {
-        font-size: 18px;
-        font-weight: 600;
-        padding: 20px 12px;
-        background-color: green;
-        color: white;
-        display: block;
-        margin-bottom: 20px;
-        border: 1px solid #d8d7d7;
-        border-radius: 8px;
-    }
-
-    #popup-hera .popup-content .pop-center .one-item a + a {
-        background-color: inherit;
-        color: #666878;
-    }
-
-    .btn-close {
-        position: absolute;
-        border: none;
-        top: -14px;
-        right: -22px;
-        cursor: pointer;
-        font-weight: bold;
-        font-size: 15px;
-        transition: .3s;
-        text-align: center;
-        background-color: transparent;
-    }
-
-    .btn-close i {
-        transition: .3s;
-        /*line-height: 28px;*/
-        text-align: center;
-        margin-right: 3px;
-        width: 100%;
-        height: 28px !important;
-        width: 28px !important;
-        border-radius: 50%;
-        background-color: #fff;
-        line-height: 27px;
-    }
-
-    .btn-close:hover i {
-        transform: rotate(360deg);
-    }
-
-    @media only screen and (max-width: 800px) {
-        #popup-hera .popup-content .pop-center {
-            width: 90%;
-            background-position: inherit;
-        }
-        #popup-hera .popup-content .pop-center .one-item h4 {
-            text-align: center;
-        }
-
-        #popup-hera .popup-content .pop-center .one-item p{
-            text-align: center;
-        }
-    }
-
 
 </style>
 
 <div class="animated fadeIn" id="popup-hera">
 
     <div class="popup-content animated bounceIn delay-1s">
-        <div class="pop-center" style="background-image:url({{URL::to('images/common/study-in-Ireland.jpg')}});">
+        <div class="pop-center" style="background-image:url({{URL::to('images/common/a1-2-600x395.jpg')}});">
             <div class="row">
                 <div class="col-md-6">
                     <div class="one-item">
-                    <h4>Thank you for visiting our website</h4>
-                    <p>How May I Help You?</p>
-                    <a href="{{URL::to('services/studying-abroad-in-ireland')}}">Get Started with Studying Abroad In Ireland</a>
-                    <a href="{{URL::to('contact.html')}}">I have a few question first!</a>
+                        <h4>Thank you for visiting our website</h4>
+                        <p>How May I Help You?</p>
+                        {{--<a href="{{URL::to('services/studying-abroad-in-ireland')}}">Get Started with Studying Abroad In Ireland</a>--}}
+                        {{--<a href="{{URL::to('contact.html')}}">I have a few questions first!</a>--}}
+                        <div class="register">
+                            <div class="content">
+                                <h4>@lang('content.home_contact_title')</h4>
+                                <div class="ip-name input-group" style="display: block">
+                                    <input name="name" type="text" placeholder="@lang('content.home_contact_name')"
+                                           class="form-control input-text"
+                                           style="display: inline-block;border-radius: unset">
+                                    <div class="invalid-feedback" style="font-size: 15px;font-weight:  bold;">
+                                        Please choose a username.
+                                    </div>
+                                </div>
+                                <div class="ip-phone input-group" style="display: block">
+                                    <input name="phone" type="text" placeholder="@lang('content.home_contact_phone')"
+                                           class="form-control input-text"
+                                           style="display: inline-block;border-radius: unset">
+                                    <div class="invalid-feedback" style="font-size: 15px;font-weight:  bold;">
+                                        Please choose a username.
+                                    </div>
+                                </div>
+                                <div class="ip-email input-group" style="display: block">
+                                    <input name="email" type="text" placeholder="@lang('content.home_contact_email')"
+                                           class="form-control input-text"
+                                           style="display: inline-block;border-radius: unset">
+                                    <div class="invalid-feedback" style="font-size: 15px;font-weight:  bold;">
+                                        Please choose a username.
+                                    </div>
+                                </div>
+                                <textarea name="description" rows="4" cols="50"
+                                          placeholder="@lang('content.home_contact_message')"></textarea>
+                                <div class="btn-submit">
+                                    {{--<a href="#">đăng ký</a>--}}
+                                    <div class="button-group">
+                                        <button type="button"
+                                                class="btn btn-contact submit-re">@lang('content.home_contact_button')<i
+                                                    class="fa fa-spinner fa-spin fa-3x fa-fw loadingSending" style="
+    font-size: 15px;display: none"></i><i
+                                                    class="fa fa-check-circle successSending" aria-hidden="true"
+                                                    style="display: none"></i></button>
+                                        <span style="display: none;color:  green;font-weight:  bold;margin-top:  5px;">Chúng tôi đã nhận được mail và sẽ phản hồi quý khách trong 24h. Xin cảm ơn.</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-6">
